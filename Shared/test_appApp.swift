4dd082e39369c2f997f3143b6857bb5053ac5ed8
Vara Denys <varayashik@gmail.com>
2022-06-19 23:41:00 +0300

    Initial commit

---
+++ Shared/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  Shared
+//
+//  Created by Deny Vorko on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
